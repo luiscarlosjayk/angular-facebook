55fd916a7eaa7c2dc50a919f3e98e40c40b09dc9
mrzmyr <m0ritz.m3yer@gmail.com>
2014-08-03 17:38:51 +0200

    chore(test): separate unit tests in directory structure

--- karma.conf.js
+++ karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function(config) {
       'bower_components/angular-mocks/angular-mocks.js',
       'lib/angular-facebook.js',
       'lib/angular-facebook-phonegap.js',
-      'test/**/*.js'
+      'test/unit/*.spec.js'
     ],
     exclude: [],
     port: 8080,
