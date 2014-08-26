9e736c65811bdb966962b8034ca086bd17f2828c
mrzmyr <m0ritz.m3yer@gmail.com>
2014-08-26 11:23:20 +0200

    chore(build): add coverage reports to karma setup and update jasmine

--- karma.conf.js
+++ karma.conf.js
@@ -9,11 +9,18 @@ module.exports = function(config) {
       'lib/angular-facebook-phonegap.js',
       'test/unit/*.spec.js'
     ],
+    preprocessors: {
+      'lib/*.js': ['coverage']
+    },
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    },
     exclude: [],
     port: 8080,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    reporters: ['progress', 'dots'],
+    reporters: ['progress', 'dots', 'coverage'],
     browsers: ['Chrome'],
     singleRun: false
   });
